c76b238d008ff33ebf0d9936ab1890499627e338
SazidWork <sazid.jobwork@gmail.com>
2026-03-15 14:48:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function append(value){
+document.getElementById("result").value+=value;
+}
+
+function calculate(){
+let result=document.getElementById("result").value;
+document.getElementById("result").value=eval(result);
+}
+
+function clearResult(){
+document.getElementById("result").value="";
+}
